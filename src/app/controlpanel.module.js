5481a6e9d56c252ee2c59361ce741dadc9b1644b
João Alexandre <corohsnk@hotmail.com>
2017-08-30 13:24:44 -0300

    removido ngRoute e highchrts

--- src/app/controlpanel.module.js
+++ src/app/controlpanel.module.js
@@ -2,8 +2,7 @@
 
     'use strict';
 
-    angular.module('controlpanel', [ 
-        'ngRoute',  
+    angular.module('controlpanel', [         
         'ui.router',  
         'ui.router.state.events',                            
         'ngDialog',
